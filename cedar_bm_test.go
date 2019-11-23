39f648769933840ce27172827f85ff4da7753570
vcaesar <vzvway@gmail.com>
2019-11-23 12:42:57 -0400

    add more benchmark code and update .gitignore

--- cedar_bm_test.go
+++ cedar_bm_test.go
@@ -17,3 +17,37 @@ func BenchmarkPrefixMatch(t *testing.B) {
 
 	tt.BM(t, fn)
 }
+
+func BenchmarkInsert(t *testing.B) {
+	fn := func() {
+		cd.Insert([]byte("a"), 1)
+		cd.Insert([]byte("b"), 3)
+		cd.Insert([]byte("d"), 6)
+	}
+
+	tt.BM(t, fn)
+}
+
+func BenchmarkJump(t *testing.B) {
+	fn := func() {
+		cd.Jump([]byte("a"), 1)
+	}
+
+	tt.BM(t, fn)
+}
+
+func BenchmarkKey(t *testing.B) {
+	fn := func() {
+		cd.Key(1)
+	}
+
+	tt.BM(t, fn)
+}
+
+func BenchmarkValue(t *testing.B) {
+	fn := func() {
+		cd.Value(1)
+	}
+
+	tt.BM(t, fn)
+}
