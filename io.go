fff23e31011adec20e700b0ae7c8d5337b68f99c
vcaesar <vzvway@gmail.com>
2020-11-21 13:59:49 -0400

    update io.go code

--- io.go
+++ io.go
@@ -35,9 +35,7 @@ func (da *Cedar) SaveToFile(fileName, dataType string) error {
 
 	out := bufio.NewWriter(file)
 	defer out.Flush()
-	da.Save(out, dataType)
-
-	return nil
+	return da.Save(out, dataType)
 }
 
 // Load loads the cedar from an io.Writer,
@@ -59,11 +57,11 @@ func (da *Cedar) Load(in io.Reader, dataType string) error {
 // where dataType is either "json" or "gob".
 func (da *Cedar) LoadFromFile(fileName, dataType string) error {
 	file, err := os.OpenFile(fileName, os.O_RDONLY, 0600)
-	defer file.Close()
-
 	if err != nil {
 		return err
 	}
+	defer file.Close()
+
 	in := bufio.NewReader(file)
 
 	return da.Load(in, dataType)
